944ebf26591e8454acd7410cc069ec38028789ec
nouma <manou@gmail.com>
2024-02-29 21:16:49 +0000

    last commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,99 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('css/about.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+
+</head>
+
+<body class="antialiased">
+
+<main>
+
+<section class="hero-us">
+    <div class="containerh jumbo">
+        <p>Diallo conciergerie</p>
+        <h1>About Us</h1>
+</div>
+</section>
+<section class="inner-us">
+    <div class="containerh ">
+ <div class="about-us">
+    <div class="BienvenueChezDiAlloConciergerieVotrePartenaireDeConfiancePourDesVoyagesInoubliablesEtDesServicesDeConciergeriePrivEAuMarocNotreExpRienceSolideNousPermetDeRPondreVosBesoinsQuIlSAgisseDeVousAccompagnerEnTantQueCoursierPrivOuDePlanifierDesVoyagesDException" style="text-align: justify; color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Bienvenue chez DI’ALLO Conciergerie, votre partenaire<br/>de confiance pour des voyages inoubliables et des<br/>services de conciergerie privée au Maroc. Notre<br/>expérience solide nous permet de répondre à vos<br/>besoins, qu'il s'agisse de vous accompagner en tant<br/>que coursier privé ou de planifier des voyages<br/>d'exception.</div>
+    <div class="Group680" style="width: 579px; height: 450px; position: relative">
+      <div class="Rectangle65" style="width: 579px; height: 450px; left: 0px; top: 0px; position: absolute; background: #DAA520; border-radius: 8px"></div>
+      <img class="Conciergerie1" style="width: 559.70px; height: 423px; left: 9.65px; top: 12px; position: absolute; border-radius: 8px" src="/images/conciergerie-mexique-decouverte 1.png" />
+    </div>
+ </div>
+ <div class="about-us">
+    <div class="Group680" style="width: 579px; height: 450px; position: relative">
+      <div class="Rectangle65" style="width: 579px; height: 450px; left: 0px; top: 0px; position: absolute; background: #DAA520; border-radius: 8px"></div>
+      <img class="Conciergerie1" style="width: 559.70px; height: 423px; left: 9.65px; top: 12px; position: absolute; border-radius: 8px" src="/images/conciergerie 1 (1).png" />
+    </div>
+    <div class="LaConciergeriePrivEChezDiAlloConciergerieNousComprenonsLImportanceDeVotreTempsEtDeVotreConfortNotreServiceDeConciergeriePrivEEtNotreServiceDeCoursierPrivGalementConnuSousLeNomDeFastTrackViseSimplifierVotreVieQuotidienneNousNousEngageonsFournirUnServiceExceptionnelQuiGarantitVotreSCuritVotreDiscrTionEtVotreConfort" style="width: 581px; text-align: justify"><span style="color: #DAA520; font-size: 36px; font-family: Poppins; font-weight: 700; word-wrap: break-word">La Conciergerie Privée<br/></span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word"><br/>Chez DI’ALLO Conciergerie, nous comprenons l'importance de votre temps et de votre confort. Notre service de conciergerie privée et notre service de coursier privé également connu sous le nom de FAST TRACK, vise à simplifier votre vie quotidienne. Nous nous engageons à fournir un service exceptionnel qui garantit votre sécurité, votre discrétion et votre confort.</span></div>
+ </div>
+ <div class="about-us">
+    <div class="GestionDuTempsNousPrenonsEnChargeLaRServationDeServicesDeNettoyageDEntretienDeLaMaisonLaCoordinationDeRendezVousMDicauxServicesPersonnalisSQueCeSoitPourDesAchatsPersonnelsDesServicesDeBienTreLOrganisationDVNementsSpCiauxNousNousOccuponsDeToutVousPermettantDeVousDTendreEtDeProfiterPleinementDeVotreVie" style="width: 581px; text-align: justify"><span style="color: #DAA520; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Gestion du Temps : </span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Nous prenons en charge<br/>• la réservation de services de nettoyage,<br/>d'entretien de la maison,<br/>• la coordination de rendez-vous médicaux.<br/></span><span style="color: #DAA520; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Services Personnalisés : </span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Que ce soit pour<br/>• Des achats personnels<br/>• Des services de bien-être<br/>• L'organisation d'événements spéciaux<br/><br/>Nous nous occupons de tout, vous permettant<br/>de vous détendre et de profiter pleinement de<br/>votre vie.</span></div>
+    <div class="Group680" style="width: 579px; height: 450px; position: relative">
+      <div class="Rectangle65" style="width: 579px; height: 450px; left: 0px; top: 0px; position: absolute; background: #DAA520; border-radius: 8px"></div>
+      <img class="Conciergerie1" style="width: 559.70px; height: 423px; left: 9.65px; top: 12px; position: absolute; border-radius: 8px" src="/images/conciergerie-mexique-decouverte 1.png" />
+    </div>
+ </div>
+ <div class="about-us">
+    <div class="Group680" style="width: 579px; height: 450px; position: relative">
+      <div class="Rectangle65" style="width: 579px; height: 450px; left: 0px; top: 0px; position: absolute; background: #DAA520; border-radius: 8px"></div>
+      <img class="Conciergerie1" style="width: 559.70px; height: 423px; left: 9.65px; top: 12px; position: absolute; border-radius: 8px" src="/images/conciergerie 1 (1).png" />
+    </div>
+
+    <div class="NotreExpertiseSETendAuDelaDeLaConciergeriePriveENousSommesEGalementDesExpertsEnPlanificationEtEnOrganisationDeVoyagesQueVousSouhaitiezExplorerLeMarocOuVoyagerPourDesBesoinsProfessionnelsNousSommesLaPourVousVoyagesDExceptionDeCouvrezLeMarocDeManieReIneDiteAvecNosItineRairesSurMesureQuiVousFerontDeCouvrirLesTreSorsCacheSDeCePaysCaptivantVoyagesDAffairesSimplifiezVosDePlacementsProfessionnelsAvecNotreServiceDeDieDeLaReServationDeVolsEtDHoTelsALaCoordinationDeReUnions" style="width: 563px; text-align: justify">
+        <div class="PlanificationEtOrganisationDeVoyages" style="text-align: justify; color: #DAA520; font-size: 24px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Planification et Organisation de Voyages</div>
+        <span style="color: black; font-size: 18px; font-family: Poppins; font-weight: 700; word-wrap: break-word"><br/>Notre expertise s'étend au-delà de la conciergerie privée. Nous sommes également des experts en planification et en organisation de voyages. Que vous souhaitiez explorer le Maroc ou voyager pour des besoins professionnels, nous sommes là pour vous.<br/></span><span style="color: #DAA520; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Voyages d'Exception</span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word"> : Découvrez le Maroc de manière inédite avec nos itinéraires sur mesure qui vous feront découvrir les trésors cachés de ce pays captivant. </span>
+    <span style="color: #DAA520; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Voyages d'Affaires</span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word"> : Simplifiez vos déplacements professionnels avec notre service dédié, de la réservation de vols et d'hôtels à la coordination de réunions.</span></div>
+ </div>
+ <div class="about-us">
+    <div class="Group692" style="width: 581px; height: 462px; position: relative">
+        <div class="NotreEquipe" style="left: 192px; top: 0px; position: absolute; text-align: justify; color: #DAA520; font-size: 24px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Notre Equipe</div>
+        <div class="NousSommesFiersDeNotreEQuipeDeVoueEComposeEDeProfessionnelsHautementQualifieSCourtoisEtReActifsChaqueMembreDeLEQuipeDiAlloConciergerieEstPassionneParLArtDeRendreVotreViePlusFacileEtVotreVoyageInoubliableVotreSatisfactionNotrePrioriteChezDiAlloConciergerieVotreSatisfactionEstNotrePrioriteAbsolueNousSommesDisponiblesAToutMomentAttentifsAVosBesoinsEtPreTsARePondreAVosMoindresDemandesNotreObjectifEstDeRendreChaqueAspectDeVotreVieQuotidiennePlusSimpleEtDeFaireDeChaqueVoyageUneExpeRienceExceptionnelle" style="width: 581px; height: 407px; left: 0px; top: 55px; position: absolute; color: black; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Nous sommes fiers de notre équipe dévouée, composée de professionnels hautement qualifiés, courtois et réactifs. Chaque membre de l'équipe DI’ALLO Conciergerie est passionné par l'art de rendre votre vie plus facile et votre voyage inoubliable. Votre Satisfaction, Notre Priorité   Chez DI’ALLO Conciergerie, votre satisfaction est notre priorité absolue. Nous sommes disponibles à tout moment, attentifs à vos besoins et prêts à répondre à vos moindres demandes. Notre objectif est de rendre chaque aspect de votre vie quotidienne plus simple et de faire de chaque voyage une expérience exceptionnelle.</div>
+      </div>
+    <div class="Group680" style="width: 579px; height: 450px; position: relative">
+      <div class="Rectangle65" style="width: 579px; height: 450px; left: 0px; top: 0px; position: absolute; background: #DAA520; border-radius: 8px"></div>
+      <img class="Conciergerie1" style="width: 559.70px; height: 423px; left: 9.65px; top: 12px; position: absolute; border-radius: 8px" src="/images/conciergerie-mexique-decouverte 1.png" />
+    </div>
+ </div>
+<p class="txt">Découvrez la différence avec DI’ALLO Conciergerie et laissez-nous vous guider vers des expériences de voyage uniques et des services de conciergerie privée qui vous permettront de gagner du temps et de vivre pleinement votre vie.
+    Contactez-nous dès aujourd'hui pour commencer à planifier vos prochains voyages et découvrir nos services de conciergerie privée exceptionnels avec DI’ALLO Conciergerie.</p>
+    <div class="img-us">
+        <img src="/images/assistive-listening-systems.png" alt="">
+        <img src="/images/like-dislike.png" alt="">
+        <img src="/images/medal-star.svg" alt="">
+        <img src="/images/human-greeting-proximity.png" alt="">
+    </div>
+
+</div>
+</section>
+
+</main>
+
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+    <!-- Styles -->
+
+</head>
+
+<body class="antialiased">
+    <main>
+        <section class="hero">
+            <div class="hero-titre containerh">
+                <h1>The Key to accessibility...</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's</p>
+                <a href="#">Join Communauty</a>
+            </div>
+        </section>
+        <section class="all-serveices ">
+            <div class="titre-all-services containerh">
+                <p>Ce que nous faisons</p>
+                <h2>Nous offrons le meilleur service</h2>
+            </div>
+            <div class="image-all-service multiple-items">
+                <div class="inner-service">
+                    <div class="img-serv">
+                        <img src="/images/Group689 .png" alt="">
+
+                    </div>
+                    <a href="#" class="frm-contact">Formulaire de contact</a>
+                </div>
+                <div class="inner-service">
+                    <div class="img-serv">
+                        <img src="/images/Group690.png" alt="">
+
+                    </div>
+                    <a href="#" class="frm-contact">Formulaire de contact</a>
+                </div>
+                <div class="inner-service">
+                    <div class="img-serv">
+                        <img src="/images/Group 691.png" alt="">
+
+                    </div>
+                    <a href="#" class="frm-contact">Formulaire de contact</a>
+                </div>
+                <div class="inner-service">
+                    <div class="img-serv">
+                        <img src="/images/Group 692.png" alt="">
+
+                    </div>
+                    <a href="#" class="frm-contact">Formulaire de contact</a>
+                </div>
+                <div class="inner-service">
+                    <div class="img-serv">
+                        <img src="/images/Group 692 8.08.13 AM.png" alt="">
+
+                    </div>
+                    <a href="#" class="frm-contact">Formulaire de contact</a>
+                </div>
+            </div>
+            <div class="video-contain containerh video-items">
+                <iframe class="embedd" src="https://www.youtube.com/embed/MTyy6BPSp2U"
+                    title="FRANCO-MAROCAINE, ELLE S&#39;INSTALLE À MARRAKECH ET CRÉE UNE CONCIERGERIE" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+                <iframe class="embedd" src="https://www.youtube.com/embed/MTyy6BPSp2U"
+                    title="FRANCO-MAROCAINE, ELLE S&#39;INSTALLE À MARRAKECH ET CRÉE UNE CONCIERGERIE" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+                <iframe class="embedd" src="https://www.youtube.com/embed/MTyy6BPSp2U"
+                    title="FRANCO-MAROCAINE, ELLE S&#39;INSTALLE À MARRAKECH ET CRÉE UNE CONCIERGERIE" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+                <iframe class="embedd" src="https://www.youtube.com/embed/MTyy6BPSp2U"
+                    title="FRANCO-MAROCAINE, ELLE S&#39;INSTALLE À MARRAKECH ET CRÉE UNE CONCIERGERIE" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+                <iframe class="embedd" src="https://www.youtube.com/embed/MTyy6BPSp2U"
+                    title="FRANCO-MAROCAINE, ELLE S&#39;INSTALLE À MARRAKECH ET CRÉE UNE CONCIERGERIE" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+            </div>
+
+        </section>
+        <section class="nsEcrire">
+            <div class="containerh">
+                <div class="styl">
+                    Envie de travailler avec nous ?</div>
+                <div class="styl1">
+                    Grâce à notre polyvalence, nous avons pu comprendre et adapter à chacun de ces domaines d'activité ;
+                    tout en identifiant leurs besoins et la meilleure</div>
+                <a href="#">Nous écrire</a>
+            </div>
+        </section>
+        <section class="nsVls">
+            <div class="nsVls-item containerh">
+                <div class="nsVl-title">
+                    <h2 class="titre-barre">Nos Valeurs</h2>
+                    <div class="allVls">
+                        <div class="Vls">
+                            <img src="/images/assistive-listening-systems.png" alt="">
+                            <h4>Ecoute</h4>
+                        </div>
+                        <div class="Vls">
+                            <img src="/images/like-dislike.png" alt="">
+                            <h4>Confiance</h4>
+                        </div>
+                        <div class="Vls">
+                            <img src="/images/medal-star.svg" alt="">
+                            <h4>Efficacite</h4>
+                        </div>
+                        <div class="Vls">
+                            <img src="images/human-greeting-proximity.png" alt="">
+                            <h4>Proximite</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="nsVl-img">
+                    <img src="/images/img-devis.png" alt="">
+                </div>
+
+            </div>
+
+        </section>
+        <section class="cntc">
+            <div class="containerh ctc">
+                <div
+                    style="color: black; font-size: 32px; font-family: Poppins; font-weight: 700; line-height: 44px; word-wrap: break-word">
+                    Nous Contacter</div>
+                <div
+                    style="color: black; font-size: 16px; font-family: Poppins; font-weight: 400; line-height: 44px; word-wrap: break-word">
+                    Nous proposons des services uniques adaptés à tout type de confort</div>
+                <div class=" contact-item">
+
+                    <div class="cntc-inner">
+
+                        <div class="styl2" style="width: 520px; height: 547px; position: relative">
+                            <div
+                                style="width: 240px; height: 65px; padding-left: 70px; padding-right: 70px; padding-top: 20px; padding-bottom: 20px; left: 0px; top: 482px; position: absolute; background: black; border-radius: 6px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+                                <div
+                                    style="text-align: center; color: #DAA520; font-size: 20px; font-family: Poppins; font-weight: 700; word-wrap: break-word">
+                                    Envoyer</div>
+                            </div>
+                            <div style="width: 520px; height: 79px; left: 0px; top: 0px; position: absolute">
+                                <input type="text" placeholder="Sophi"
+                                    style="width: 520px; height: 60px; left: 0px; top: 19px; position: absolute; background: white; border-radius: 8px; border: 1px #595959 solid">
+                                <div
+                                    style="left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 600; text-transform: uppercase; word-wrap: break-word">
+                                    Prénom & Nom</div>
+                            </div>
+                            <div style="width: 520px; height: 79px; left: 0px; top: 99px; position: absolute">
+                                <input type="email" placeholder="exemple@mail.com"
+                                    style="width: 520px; height: 60px; left: 0px; top: 19px; position: absolute; background: white; border-radius: 8px; border: 1px #C5D0EA solid">
+                                <div
+                                    style="left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 600; text-transform: uppercase; word-wrap: break-word">
+                                    Email</div>
+                            </div>
+                            <div style="width: 520px; height: 79px; left: 0px; top: 198px; position: absolute">
+                                <input type="date" placeholder="21 Mai 2023 -09H-30"
+                                    style="width: 520px; height: 60px; left: 0px; top: 19px; position: absolute; background: white; border-radius: 8px; border: 1px #C5D0EA solid">
+                                <div
+                                    style="left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 600; text-transform: uppercase; word-wrap: break-word">
+                                    Choisir la date</div>
+                            </div>
+                            <div style="width: 520px; height: 164px; left: 0px; top: 297px; position: absolute">
+                                <textarea placeholder="Votre message"
+                                    style="width: 520px; height: 145px; left: 0px; top: 19px; position: absolute; background: white; border-radius: 8px; border: 1px #C5D0EA solid"></textarea>
+                                <div
+                                    style="left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 600; text-transform: uppercase; word-wrap: break-word">
+                                    Message</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="inner-cntc">
+                        <img src="/images/blog info contact 3.59.52 PM.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript">
+
+        $('.video-items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            prevArrow: '<img class="slick-prev" src="/images/arrow-left.png">',
+            nextArrow: '<img class="slick-next" src="/images/arrow-right.png">',
+            responsive: [
+                {
+                    breakpoint: 1550,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 1270,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 456,
+                    settings: {
+                        slidesToShow: 1,
+
+                    }
+                },
+
+
+
+            ]
+        });
+    </script>
+    <script type="text/javascript">
+
+
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            responsive: [
+                {
+                    breakpoint: 1550,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 1170,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 780,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 450,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 400,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+                ,
+                {
+                    breakpoint: 350,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 300,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+    </script>
+</body>
+
+</html>
